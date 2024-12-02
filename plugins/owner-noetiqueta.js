556efa0b6269dc98a87b03a607c949b4cf317677
Prakharper <158788969+Prakharper@users.noreply.github.com>
2024-12-02 13:31:17 -0400

    Actualizar owner-noetiqueta.js

--- plugins/owner-noetiqueta.js
+++ plugins/owner-noetiqueta.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-await m.react('💛')
+await m.react('🩸')
 await conn.reply(m.chat, `Hola @${m.sender.split`@`[0]} si necesitas la ayuda de mi creador porfavor escribele al privado\n*- Solo asuntos importantes -*`, estilo, { mentions: [m.sender] })
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おWILLZEK;;\nFN:おWILLZEK\nORG:おWILLZEK\nTITLE:\nitem1.TEL;waid=50557865603:50557865603\nitem1.X-ABLabel:おWILLZEK\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おWILLZEK\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'おWILLZEK', contacts: [{ vcard }] }}, {quoted: m})
