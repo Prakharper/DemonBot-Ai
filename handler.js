c2dcebe05dd56871245a0def94a35e5dfdaa5225
Prakharper <158788969+Prakharper@users.noreply.github.com>
2024-12-07 14:07:14 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -413,7 +413,7 @@ global.dfail = (type, m, conn, usedPrefix) => {
         private: "> _*`Ve a mi chat privado y usa este comando`*_",
         admin: "> _*`¿Quién mierda eres? No eres administrador no jodas`*_",
         botAdmin: "> _*`Es necesario que sea admin primero para usar esta función`*_",
-        unreg: "> _*`‼️USUARIO NO REGISTRADO‼️`*_\n\n`Para registrarse:`\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg WillZek. 18",
+        unreg: "> _*`‼️USUARIO NO REGISTRADO‼️`*_\n\n`Para registrarse:`\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg prakharper. 23",
         restrict: "> _*`Comando desactivado por mi creador`*_" 
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
