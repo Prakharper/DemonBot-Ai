a3a19804e0074a8563a7b625b80e5950c159c32d
Niño Piña <ninopina10@gmail.com>
2024-12-09 21:59:58 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -48,7 +48,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     }
   } catch (e) {
     console.error(e)
-    m.reply('Error')
+   await return m.reply(m.chat 'Error {error.message}, m');
   }
 }
 
