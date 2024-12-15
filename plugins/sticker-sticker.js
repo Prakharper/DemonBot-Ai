05072ebef05baae4815276b8a0b9e1d04a1aa228
Niño Piña <ninopina10@gmail.com>
2024-12-15 14:57:31 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -44,11 +44,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }
       m.reply(stiker)
     } else {
-      m.reply('     `✧ Responde a una Imagen o Video.`')
+      conn.reply(m.chat, '    `✧ Responde a una Imagen o Video.`', m, rcanal)
     }
   } catch (e) {
     console.error(e)
-   await return m.reply(m.chat, 'Error {error.message}', m);
+    m.reply('Error')
   }
 }
 
